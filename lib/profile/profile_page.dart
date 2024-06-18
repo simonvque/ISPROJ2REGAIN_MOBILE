9d34d7439d125c2dea4661fce4e837e8959f0970
Stannyy222 <stanholley.huerto@benilde.edu.ph>
2024-06-18 16:30:04 +0800

    fixed profile menu, colors, icons

--- lib/profile/profile_page.dart
+++ lib/profile/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:regain_mobile/constants/colors.dart';
+import 'package:regain_mobile/constants/image_strings.dart';
 import 'package:regain_mobile/profile/app_bar.dart';
 import 'package:regain_mobile/profile/edit_profile_page.dart';
 import 'package:regain_mobile/profile/profile_menu.dart';
@@ -9,63 +11,48 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, 'My profile'),
-      body: const UserProfile(),
-    );
-  }
-}
-
-class UserProfile extends StatelessWidget {
-  const UserProfile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
     final user = UserData().myUser;
 
-    return Container(
-      alignment: Alignment.center,
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 120,
-            width: 120,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image(
-                  image: AssetImage(user.imagePath),
-                  fit: BoxFit.cover,
-                )),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          StyleUsernameTxt('@${user.username}'),
-          StyleBodyTxt(user.contactNum),
-          const SizedBox(
-            height: 8,
-          ),
-          SizedBox(
-              width: 222,
-              child: FilledButton(
-                  style: FilledButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8))),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const EditProfilePage()));
-                  },
-                  child: const Text('Edit profile'))),
-          const SizedBox(
-            height: 30,
-          ),
-          const ProfileMenu()
-        ],
+    return Scaffold(
+      appBar: buildAppBar(context, 'My profile'),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage(ReGainImages.exProfilePic),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            StyleUsernameTxt('@${user.username}'),
+            StyleBodyTxt(user.contactNum),
+            const SizedBox(
+              height: 8,
+            ),
+            SizedBox(
+                width: 222,
+                child: FilledButton(
+                    style: FilledButton.styleFrom(
+                        backgroundColor: green,
+                        foregroundColor: white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8))),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const EditProfilePage()));
+                    },
+                    child: const Text('Edit profile'))),
+            const SizedBox(
+              height: 30,
+            ),
+            const ProfileMenu()
+          ],
+        ),
       ),
     );
   }
--- lib/profile/seller_profile_page.dart
+++ lib/profile/seller_profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:regain_mobile/constants/colors.dart';
 import 'package:regain_mobile/profile/profile_page.dart';
 import 'package:regain_mobile/profile/util/user_data.dart';
 import 'app_bar.dart';
@@ -9,66 +10,66 @@ import 'app_bar.dart';
 
 */
 class SellerProfilePage extends StatelessWidget {
-  const SellerProfilePage ({super.key});
+  const SellerProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-     final user = UserData().myUser;
+    final user = UserData().myUser;
 
     return Scaffold(
-      appBar: buildAppBar(context, '',
-      showReportButton: true),
+      appBar: buildAppBar(context, '', showReportButton: true),
       body: Stack(
         clipBehavior: Clip.none,
         children: [
           //Default CoverPhoto
           Container(
-          width: MediaQuery.of(context).size.width * 1,
-          height: MediaQuery.of(context).size.height * 0.10,
-          color: Colors.green, 
-            ),
+            width: MediaQuery.of(context).size.width * 1,
+            height: MediaQuery.of(context).size.height * 0.10,
+            color: green,
+          ),
 
           //Profile Picture and Info
           Positioned(
-            bottom: MediaQuery.of(context).size.height * -0.17, 
-            left: MediaQuery.of(context).size.width * 0.05, 
+            bottom: MediaQuery.of(context).size.height * -0.17,
+            left: MediaQuery.of(context).size.width * 0.05,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-              CircleAvatar(
-                  radius: 55, 
-                  backgroundColor: Colors.white, 
+                CircleAvatar(
+                  radius: 55,
+                  backgroundColor: white,
                   child: CircleAvatar(
-                      radius: 52,
-                      backgroundImage: AssetImage(user.imagePath),
-                    ),
+                    radius: 52,
+                    backgroundImage: AssetImage(user.imagePath),
                   ),
-                 Row(
+                ),
+                Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [ 
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const StyleUsernameTxt('Samantha Jones'),     
-                          StyleBodyTxt('@${user.username}'),
-                        ],
-                      ), 
-                      const SizedBox(width: 100,),
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          StyleBodyTxt('Joined June 2024'),
-                          SizedBox(height: 5),
-                          StyleBodyTxt('Verified')
-                        ],
-                      ),
-                    ],
-                 ),
-                 const SizedBox(height: 4),
-                 const StyleBodyTxt('4.2'),
-                 //const StyleBodyTxt('junkshop name'),
-                ],
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const StyleUsernameTxt('Samantha Jones'),
+                        StyleBodyTxt('@${user.username}'),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 100,
+                    ),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StyleBodyTxt('Joined June 2024'),
+                        SizedBox(height: 5),
+                        StyleBodyTxt('Verified')
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                const StyleBodyTxt('4.2'),
+                //const StyleBodyTxt('junkshop name'),
+              ],
             ),
           ),
         ],
