4b90cece0d07b61334d0b2c8a48d34400d4c01db
Stannyy222 <stanholley.huerto@benilde.edu.ph>
2024-06-20 17:07:37 +0800

    added new buttons in edit profile/profile menu; added sizes,spaces in constants

--- lib/profile/profile_menu.dart
+++ lib/profile/profile_menu.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:regain_mobile/constants/colors.dart';
+import 'package:regain_mobile/constants/sizes.dart';
+import 'package:regain_mobile/constants/text_strings.dart';
 import 'package:regain_mobile/profile/combal_page.dart';
 import 'package:regain_mobile/profile/contactus_page.dart';
 import 'package:regain_mobile/profile/seller_profile_page.dart';
@@ -17,53 +20,65 @@ class ProfileMenu extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const StyleLabelTxt('PROFILE MENU'),
-      
+        Text(
+          ReGainTexts.lblProfileMenu,
+          style: Theme.of(context).textTheme.labelMedium?.copyWith(color: gray),
+        ),
+        RegainButtons(
+          text: 'Manage listings',
+          onPressed: () {
+            //add code
+          },
+          type: ButtonType.transparentOutlined,
+          size: ButtonSize.large,
+          leftIcon: CupertinoIcons.collections,
+          rightIcon: CupertinoIcons.chevron_forward,
+        ),
         RegainButtons(
           text: 'Transaction History',
-          onPressed: (){
-            Navigator.push(context,
-                  MaterialPageRoute(
-                      builder: (context) => TransactionsPage(
-                            transactions: transactions,),
-                          ),
-                          );
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => TransactionsPage(
+                  transactions: transactions,
+                ),
+              ),
+            );
           },
           type: ButtonType.transparentOutlined,
           size: ButtonSize.large,
           leftIcon: CupertinoIcons.doc_on_clipboard,
           rightIcon: CupertinoIcons.chevron_forward,
-          ),
-
+        ),
         RegainButtons(
           text: 'Commission balance',
-          onPressed: (){
-            Navigator.push(context,
-                  MaterialPageRoute(
-                      builder: (context) => const CommissionPage(),
-                          ),
-                          );
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const CommissionPage(),
+              ),
+            );
           },
           type: ButtonType.transparentOutlined,
           size: ButtonSize.large,
           leftIcon: CupertinoIcons.creditcard,
           rightIcon: CupertinoIcons.chevron_forward,
-          ),
-
+        ),
         RegainButtons(
           text: 'Penalty points',
-          onPressed: (){
+          onPressed: () {
             //penalty points page
           },
           type: ButtonType.transparentOutlined,
           size: ButtonSize.large,
           leftIcon: CupertinoIcons.exclamationmark_circle,
           rightIcon: CupertinoIcons.chevron_forward,
-          ),
-
+        ),
         RegainButtons(
           text: 'Settings',
-          onPressed: (){
+          onPressed: () {
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => const SettingsPage()));
           },
@@ -71,22 +86,20 @@ class ProfileMenu extends StatelessWidget {
           size: ButtonSize.large,
           leftIcon: CupertinoIcons.settings,
           rightIcon: CupertinoIcons.chevron_forward,
-          ),
-
+        ),
         RegainButtons(
           text: 'FAQs',
-          onPressed: (){
+          onPressed: () {
             //FAQs page
           },
           type: ButtonType.transparentOutlined,
           size: ButtonSize.large,
           leftIcon: CupertinoIcons.question_circle,
           rightIcon: CupertinoIcons.chevron_forward,
-          ),
-          
+        ),
         RegainButtons(
           text: 'Contact us',
-          onPressed: (){
+          onPressed: () {
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => const ContactUsPage()));
           },
@@ -94,20 +107,21 @@ class ProfileMenu extends StatelessWidget {
           size: ButtonSize.large,
           leftIcon: CupertinoIcons.chat_bubble_2,
           rightIcon: CupertinoIcons.chevron_forward,
-          ),
-
+        ),
         const SizedBox(
-          height: 12,
+          height: ReGainSizes.largeSpace,
         ),
         RegainButtons(
           text: 'Logout',
-          onPressed: (){
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const SellerProfilePage()));
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const SellerProfilePage()));
           },
           type: ButtonType.filled,
           size: ButtonSize.large,
-          ),
+        ),
       ],
     );
   }
--- lib/themes/text_theme.dart
+++ lib/themes/text_theme.dart
@@ -2,25 +2,40 @@ import 'package:flutter/material.dart';
 
 import '../constants/colors.dart';
 
-class ReGainTextTheme{
+class ReGainTextTheme {
   ReGainTextTheme._();
 
   static TextTheme lightTextTheme({Color color = black}) {
     return TextTheme(
-      headlineLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold, color: color),
-      headlineMedium: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600, color: color),
-      headlineSmall: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600, color: color),
+      //For emphasis, big solo text
+      headlineLarge:
+          TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold, color: color),
+      headlineMedium:
+          TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600, color: color),
+      headlineSmall:
+          TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600, color: color),
 
-      titleLarge: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600, color: color),
-      titleMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500, color: color),
-      titleSmall: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: color),
+      //For content title
+      titleLarge:
+          TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600, color: color),
+      titleMedium:
+          TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500, color: color),
+      titleSmall:
+          TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: color),
 
-      bodyLarge: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: color),
-      bodyMedium: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal, color: color),
-      bodySmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500, color: color),
+      //for body content, button texts
+      bodyLarge:
+          TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: color),
+      bodyMedium: TextStyle(
+          fontSize: 14.0, fontWeight: FontWeight.normal, color: color),
+      bodySmall:
+          TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500, color: color),
 
-      labelLarge: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold, color: color),
-      labelMedium: TextStyle(fontSize: 12.0, fontWeight: FontWeight.normal, color: color),
+      //for subtitle, small texts
+      labelLarge:
+          TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold, color: color),
+      labelMedium: TextStyle(
+          fontSize: 12.0, fontWeight: FontWeight.normal, color: color),
     );
   }
 }
