d4091ffc93ab99d3b184d0f21b1fbb82a3a4d37a
“ashleyyuloo” <ashleyyulo@github.com>
2024-06-26 14:49:02 +0800

    fixed text styles

--- lib/themes/elements/input fields/regain_textbox.dart
+++ lib/themes/elements/input fields/regain_textbox.dart
@@ -6,14 +6,17 @@ class RegainTextbox extends StatelessWidget {
   final String? labelText;
   final String? hintText;
   final Color focusedBorderColor;
+  final Color borderColor;
   final Color fontColor;
   final Color iconColor;
   final Color fillColor;
+  final Color hintAndLabelTextColor;
   final TextInputType keyboardType;
   final Widget? prefixIcon;
   final Widget? suffixIcon;
   final bool isUnderlineBorder;
   final TextEditingController? controller;
+  final bool obscureText;
 
   const RegainTextbox({
     Key? key,
@@ -21,13 +24,16 @@ class RegainTextbox extends StatelessWidget {
     this.labelText,
     this.hintText,
     this.focusedBorderColor = green,
+    this.borderColor = white,
     this.fontColor = black,
-    this.iconColor = black,
+    this.iconColor = Colors.grey,
     this.fillColor = white,
+    this.hintAndLabelTextColor = Colors.grey,
     this.keyboardType = TextInputType.text,
     this.prefixIcon,
     this.suffixIcon,
-    this.isUnderlineBorder = false, // Default to border on all sides
+    this.isUnderlineBorder = false,
+    this.obscureText = false,
   }) : super(key: key);
 
   @override
@@ -35,39 +41,41 @@ class RegainTextbox extends StatelessWidget {
     return TextFormField(
       controller: controller,
       keyboardType: keyboardType,
+      obscureText: obscureText,
       style: TextStyle(color: fontColor),
       decoration: InputDecoration(
         labelText: labelText,
+        labelStyle: TextStyle(color: hintAndLabelTextColor),
         hintText: hintText,
-        hintStyle: TextStyle(color: fontColor),
+        hintStyle: TextStyle(color: hintAndLabelTextColor),
         prefixIcon: prefixIcon != null
             ? IconTheme(
-                data: IconThemeData(color: iconColor),
-                child: prefixIcon!,
-              )
+          data: IconThemeData(color: iconColor),
+          child: prefixIcon!,
+        )
             : null,
         suffixIcon: suffixIcon != null
             ? IconTheme(
-                data: IconThemeData(color: iconColor),
-                child: suffixIcon!,
-              )
+          data: IconThemeData(color: iconColor),
+          child: suffixIcon!,
+        )
             : null,
         filled: true,
         fillColor: fillColor,
         border: isUnderlineBorder
             ? UnderlineInputBorder(
-                borderSide: BorderSide(color: focusedBorderColor),
-              )
+          borderSide: BorderSide(color: focusedBorderColor),
+        )
             : OutlineInputBorder(
-                borderSide: BorderSide(color: focusedBorderColor),
-              ),
+          borderSide: BorderSide(color: focusedBorderColor),
+        ),
         focusedBorder: isUnderlineBorder
             ? UnderlineInputBorder(
-                borderSide: BorderSide(color: focusedBorderColor),
-              )
+          borderSide: BorderSide(color: focusedBorderColor),
+        )
             : OutlineInputBorder(
-                borderSide: BorderSide(color: focusedBorderColor),
-              ),
+          borderSide: BorderSide(color: focusedBorderColor),
+        ),
       ),
     );
   }
