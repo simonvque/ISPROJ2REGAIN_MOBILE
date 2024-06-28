32ab628391b72bb5b8bea02a8cc97595a4464253
“ashleyyuloo” <ashleyyulo@github.com>
2024-06-28 17:59:38 +0800

    fixed textbox

--- lib/nav.dart
+++ lib/nav.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:regain_mobile/add.dart';
 import 'package:regain_mobile/features/screens/chatfeatures/inbox.dart';
 import 'package:regain_mobile/features/screens/favorites.dart';
 import 'package:regain_mobile/features/screens/filter.dart';
 import 'package:regain_mobile/features/screens/homepage/homepage.dart';
-import 'package:regain_mobile/profile/profile_page.dart';
 
+import 'add/add.dart';
 class NavigationMenu extends StatefulWidget {
   const NavigationMenu({Key? key}) : super(key: key);
 
--- lib/themes/elements/input fields/regain_textbox.dart
+++ lib/themes/elements/input fields/regain_textbox.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+
 import '../../../constants/colors.dart';
 
 class RegainTextbox extends StatelessWidget {
@@ -8,11 +10,13 @@ class RegainTextbox extends StatelessWidget {
   final Color fontColor;
   final Color iconColor;
   final Color fillColor;
+  final Color hintAndLabelTextColor;
   final TextInputType keyboardType;
   final Widget? prefixIcon;
   final Widget? suffixIcon;
   final bool isUnderlineBorder;
   final TextEditingController? controller;
+  final bool obscureText;
 
   const RegainTextbox({
     Key? key,
@@ -21,12 +25,14 @@ class RegainTextbox extends StatelessWidget {
     this.hintText,
     this.focusedBorderColor = green,
     this.fontColor = black,
-    this.iconColor = black,
+    this.iconColor = Colors.grey,
     this.fillColor = white,
+    this.hintAndLabelTextColor = Colors.grey,
     this.keyboardType = TextInputType.text,
     this.prefixIcon,
     this.suffixIcon,
-    this.isUnderlineBorder = false, // Default to border on all sides
+    this.isUnderlineBorder = false,
+    this.obscureText = false,
   }) : super(key: key);
 
   @override
@@ -34,39 +40,41 @@ class RegainTextbox extends StatelessWidget {
     return TextFormField(
       controller: controller,
       keyboardType: keyboardType,
+      obscureText: obscureText,
       style: TextStyle(color: fontColor),
       decoration: InputDecoration(
         labelText: labelText,
+        labelStyle: TextStyle(color: hintAndLabelTextColor),
         hintText: hintText,
-        hintStyle: TextStyle(color: fontColor),
+        hintStyle: TextStyle(color: hintAndLabelTextColor),
         prefixIcon: prefixIcon != null
             ? IconTheme(
-                data: IconThemeData(color: iconColor),
-                child: prefixIcon!,
-              )
+          data: IconThemeData(color: iconColor),
+          child: prefixIcon!,
+        )
             : null,
         suffixIcon: suffixIcon != null
             ? IconTheme(
-                data: IconThemeData(color: iconColor),
-                child: suffixIcon!,
-              )
+          data: IconThemeData(color: iconColor),
+          child: suffixIcon!,
+        )
             : null,
         filled: true,
         fillColor: fillColor,
         border: isUnderlineBorder
             ? UnderlineInputBorder(
-                borderSide: BorderSide(color: focusedBorderColor),
-              )
+          borderSide: BorderSide(color: focusedBorderColor),
+        )
             : OutlineInputBorder(
-                borderSide: BorderSide(color: focusedBorderColor),
-              ),
+          borderSide: BorderSide(color: focusedBorderColor),
+        ),
         focusedBorder: isUnderlineBorder
             ? UnderlineInputBorder(
-                borderSide: BorderSide(color: focusedBorderColor),
-              )
+          borderSide: BorderSide(color: focusedBorderColor),
+        )
             : OutlineInputBorder(
-                borderSide: BorderSide(color: focusedBorderColor),
-              ),
+          borderSide: BorderSide(color: focusedBorderColor),
+        ),
       ),
     );
   }
