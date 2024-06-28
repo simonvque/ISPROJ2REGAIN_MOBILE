b0168f51e26d631e0d793543ff63c07d5d44e8cd
“ashleyyuloo” <ashleyyulo@github.com>
2024-06-28 17:18:12 +0800

    added filter, favorites, inbox screens.

--- lib/features/screens/login/widgets/login_form.dart
+++ lib/features/screens/login/widgets/login_form.dart
@@ -1,16 +1,13 @@
-import 'dart:convert';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
-
 import 'package:flutter/material.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:regain_mobile/nav.dart';
+
 import 'package:regain_mobile/themes/elements/button_styles.dart';
 
 import '../../../../constants/colors.dart';
 import '../../../../constants/sizes.dart';
 import '../../../../constants/text_strings.dart';
-import '../../profile/seller_profile_page.dart';
+import '../../../../profile/seller_profile_page.dart';
 import '../../../../themes/elements/input fields/password_textbox.dart';
 import '../../../../themes/elements/input fields/regain_textbox.dart';
 import '../../forgotPassword/forgot_password.dart';
@@ -18,93 +15,16 @@ import '../../homepage/homepage.dart';
 import '../../registration/registration.dart';
 
 class LoginForm extends StatefulWidget {
-  const LoginForm({Key? key}) : super(key: key);
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
 
   @override
   _LoginFormState createState() => _LoginFormState();
 }
 
 class _LoginFormState extends State<LoginForm> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
-  bool isLoading = false;
-
-  Future<void> _login() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    final loginSuccessful = await _attemptLogin(
-      _usernameController.text,
-      _passwordController.text,
-    );
-
-    setState(() {
-      isLoading = false;
-    });
-
-    if (loginSuccessful) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const NavigationMenu()),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-            content: Text('Login failed. Please check your credentials.')),
-      );
-    }
-  }
-
-  Future<bool> _attemptLogin(String username, String password) async {
-    try {
-      final response = await http.post(
-        Uri.parse(
-            'http://10.0.2.2:3000/login'), // Use 10.0.2.2 for Android emulator
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(<String, String>{
-          'username': username,
-          'password': password,
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        final token = json.decode(response.body)['token'];
-        await _secureStorage.write(key: 'token', value: token);
-        return true;
-      } else {
-        return false;
-      }
-    } catch (e) {
-      print('Error during login: $e');
-      return false;
-    }
-  }
-
-  void _onLoginPressed() {
-    if (!isLoading && _validateInputs()) {
-      _login();
-    }
-  }
-
-  bool _validateInputs() {
-    if (_usernameController.text.trim().isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Username cannot be empty.')),
-      );
-      return false;
-    }
-    if (_passwordController.text.trim().isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Password cannot be empty.')),
-      );
-      return false;
-    }
-    return true;
-  }
+  bool isPasswordVisible = false; // Track whether password is visible or not
 
   @override
   Widget build(BuildContext context) {
@@ -113,30 +33,28 @@ class _LoginFormState extends State<LoginForm> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           // Username
-          RegainTextbox(
-            controller: _usernameController,
-            labelText: ReGainTexts.logIn,
-            isUnderlineBorder: true,
-          ),
+          const RegainTextbox(labelText: ReGainTexts.username, isUnderlineBorder: true),
+          // TextFormField(
+          //   decoration: const InputDecoration(labelText: ReGainTexts.logIn),
+          // ),
           const SizedBox(height: ReGainSizes.spaceBtwInputFields),
 
           // Password
-          PasswordTextFormField(
-            controller: _passwordController,
-            labelText: ReGainTexts.password,
-          ),
+          const PasswordTextFormField(labelText: ReGainTexts.password, isUnderlineBorder: true),
+          // TextFormField(
+          //   decoration: const InputDecoration(labelText: ReGainTexts.password, suffixIcon: Icon(Iconsax.eye_slash)),
+          // ),
           const SizedBox(height: ReGainSizes.spaceBtwInputFields / 2),
 
           // Forgot Password
           Row(
             children: [
-              const Spacer(),
+              Spacer(), // Pushes the "Forgot Password" text to the right
               TextButton(
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(
-                        builder: (context) => ForgetPasswordScreen()),
+                    MaterialPageRoute(builder: (context) => ForgetPasswordScreen()),
                   );
                 },
                 child: Text(
@@ -150,18 +68,16 @@ class _LoginFormState extends State<LoginForm> {
 
           // Login button
           RegainButtons(
-            text: isLoading ? ' ' : ReGainTexts.logIn,
-            onPressed: _onLoginPressed,
+            text: ReGainTexts.logIn,
+            onPressed: (){
+              // --- Add validation ---
+
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const NavigationMenu()));
+            },
             type: ButtonType.filled,
             size: ButtonSize.large,
           ),
-
-          // Loading indicator
-          if (isLoading)
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Center(child: CircularProgressIndicator()),
-            ),
 
           // Sign up
           Padding(
@@ -177,8 +93,7 @@ class _LoginFormState extends State<LoginForm> {
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(
-                          builder: (context) => RegistrationScreen()),
+                      MaterialPageRoute(builder: (context) => RegistrationScreen()),
                     );
                   },
                   child: Text(
