05769833194fc76d2f3c041a27abd659d787eeef
raelactao <rvlpics@gmail.com>
2024-07-15 10:45:46 +0800

    added import statements

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:regain_mobile/features/screens/profile/profile_page.dart';
+import 'package:regain_mobile/provider/address_data_provider.dart';
 
 import 'package:regain_mobile/provider/app_data_provider.dart';
 import 'package:regain_mobile/provider/category_data_provider.dart';
