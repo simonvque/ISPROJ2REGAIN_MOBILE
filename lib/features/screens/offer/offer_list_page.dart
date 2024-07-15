ecf169b7b949b34c329649048258fb0e332065aa
stannyy222 <stanholley.huerto@benilde.edu.ph>
2024-07-16 02:33:28 +0800

    offer list completed; with temporary product model

--- lib/features/screens/offer/offer_list_page.dart
+++ lib/features/screens/offer/offer_list_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:regain_mobile/constants/colors.dart';
+import 'package:regain_mobile/features/screens/offer/offer_tile/buyer_offer_tile.dart';
+import 'package:regain_mobile/features/screens/offer/seller_offerlist_page.dart';
 import 'package:regain_mobile/features/screens/profile/app_bar.dart';
-import 'package:regain_mobile/themes/elements/button_styles.dart';
-import 'package:regain_mobile/themes/elements/input%20fields/regain_textbox.dart';
+
+import 'temp_view_product.dart'; //temporary viewProduct Class 
 
 class OfferListPage extends StatelessWidget {
   OfferListPage({super.key});
@@ -61,186 +63,15 @@ class OfferListPage extends StatelessWidget {
               itemCount: sampleProducts.length,
               itemBuilder: (context, index) {
                 final product = sampleProducts[index];
-                return ProductOfferTile(product: product);
+                return BuyerOfferTile(product: product);
               },
             ),
-            ListView.builder(
-              itemCount: sampleProducts.length,
-              itemBuilder: (context, index) {
-                final product = sampleProducts[index];
-                return ProductOfferTile(product: product);
-              },
+           
+            SellerOfferList() // seller side
+          ]
             ),
-          ],
         ),
-      ),
-    );
+      );
   }
 }
 
-class ProductOfferTile extends StatefulWidget {
-  final ViewProduct product;
-
-  const ProductOfferTile({super.key, required this.product});
-
-  @override
-  ProductOfferTileState createState() => ProductOfferTileState();
-}
-
-class ProductOfferTileState extends State<ProductOfferTile> {
-  bool _isExpanded = false;
-  final TextEditingController _offerController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _offerController.text = widget.product.price;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(4.0),
-      padding: const EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-          color: Colors.transparent,
-          borderRadius: BorderRadius.circular(10.0),
-          border: const Border(bottom: BorderSide(color: gray, width: 1.0))),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Status: ${widget.product.offerStatus}',
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-          const SizedBox(height: 8),
-          Text(
-            '${widget.product.productName} - PHP ${widget.product.price}',
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-          const SizedBox(height: 8),
-          Text(
-            widget.product.sellerUsername,
-            style: Theme.of(context).textTheme.labelLarge,
-          ),
-          const SizedBox(height: 16),
-          Text(
-            'My Offer: PHP ${widget.product.price}',
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-
-          const SizedBox(height: 22),
-
-          //conditional for offer status
-          if (!_isExpanded && widget.product.offerStatus == 'Pending')
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                RegainButtons(
-                  text: 'Update Offer',
-                  onPressed: () {
-                    setState(() {
-                      _isExpanded = true;
-                    });
-                  },
-                  type: ButtonType.text,
-                  size: ButtonSize.small,
-                  txtSize: BtnTxtSize.medium,
-                ),
-                RegainButtons(
-                  text: 'Cancel Offer',
-                  onPressed: () {
-                    // Add logic to cancel the offer
-                  },
-                  type: ButtonType.text,
-                  size: ButtonSize.small,
-                  txtSize: BtnTxtSize.medium,
-                ),
-              ],
-            ),
-          if (_isExpanded)
-            Column(
-              children: [
-                RegainTextbox(
-                  controller: _offerController,
-                  keyboardType: TextInputType.number,
-                  labelText: "Enter your new offer:",
-                  focusedBorderColor: green,
-                  isUnderlineBorder: true,
-                ),
-                const SizedBox(height: 12),
-                Row(
-                  children: [
-                    RegainButtons(
-                      text: 'Cancel',
-                      onPressed: () {
-                        setState(() {
-                          _isExpanded = false;
-                        });
-                      },
-                      type: ButtonType.text,
-                      size: ButtonSize.small,
-                      txtSize: BtnTxtSize.medium,
-                    ),
-                    const SizedBox(width: 6),
-                    RegainButtons(
-                      text: 'Save offer',
-                      onPressed: () {
-                        setState(() {
-                          widget.product.price = _offerController.text;
-                          _isExpanded = false;
-                          // Add additional logic when the offer is updated
-                        });
-                      },
-                      type: ButtonType.filled,
-                      size: ButtonSize.small,
-                      txtSize: BtnTxtSize.medium,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          if (widget.product.offerStatus == 'Accepted')
-            Align(
-              alignment: Alignment.center,
-              child: RegainButtons(
-                text: 'Proceed to Checkout',
-                onPressed: () {
-                  // Add logic to proceed to checkout
-                },
-                type: ButtonType.filled,
-                size: ButtonSize.medium,
-                txtSize: BtnTxtSize.large,
-              ),
-            )
-        ],
-      ),
-    );
-  }
-}
-
-class ViewProduct {
-  int productID;
-  String productName;
-  String location;
-  String price;
-  String sellerUsername;
-  String weight;
-  String category;
-  bool canDeliver;
-  bool isFavorite;
-  String offerStatus;
-
-  ViewProduct({
-    required this.productID,
-    required this.productName,
-    required this.location,
-    required this.price,
-    required this.sellerUsername,
-    required this.weight,
-    required this.category,
-    required this.canDeliver,
-    required this.isFavorite,
-    required this.offerStatus,
-  });
-}
