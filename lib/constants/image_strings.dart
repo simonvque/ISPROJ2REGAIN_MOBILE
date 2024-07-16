9df7daab2131c201a2717b44dd62dcfb7b667956
raelactao <rvlpics@gmail.com>
2024-07-16 23:39:31 +0800

    onboarding

--- lib/constants/image_strings.dart
+++ lib/constants/image_strings.dart
@@ -1,12 +1,16 @@
-class ReGainImages{
+class ReGainImages {
   //onboarding images
-  static const String onboardingImage1 = "assets/images/onboarding/onboarding1.png";
-  static const String onboardingImage2 = "assets/images/onboarding/onboarding2.png";
-  static const String onboardingImage3 = "assets/images/onboarding/onboarding3.png";
-  static const String onboardingImage4 = "assets/images/onboarding/onboarding4.png";
+  static const String onboardingImage1 =
+      "assets/images/onboarding/onboarding1.png";
+  static const String onboardingImage2 =
+      "assets/images/onboarding/onboarding2.png";
+  static const String onboardingImage3 =
+      "assets/images/onboarding/onboarding3.png";
+  static const String onboardingImage4 =
+      "assets/images/onboarding/onboarding4.png";
 
   //logo
-  static const String appLogo = "assets/images/logo/logo.png";
+  static const String appLogo = "assets/images/logo/regain.png";
   static const String shield = "assets/images/registration/shieldcheck.png";
 
   //homepage
@@ -14,6 +18,6 @@ class ReGainImages{
   static const String plastic = "assets/images/homepage/plastic.png";
 
   //profile
-    static const String exProfilePic = "assets/images/profile/profileSam.jpg";
-    static const String exProductPic = "assets/images/profile/plastic_straw.png";
+  static const String exProfilePic = "assets/images/profile/profileSam.jpg";
+  static const String exProductPic = "assets/images/profile/plastic_straw.png";
 }
