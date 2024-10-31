42bf1af26cd260229a9afe699452230a91a635e3
simonvque <simon.que@benilde.edu.ph>
2024-10-31 15:20:21 +0800

    lulu

--- lib/features/screens/offer/offerpopup.dart
+++ lib/features/screens/offer/offerpopup.dart
@@ -157,7 +157,7 @@ class _OfferPricePopupState extends State<OfferPricePopup> {
             alignment: Alignment.center,
             child: Container(
               margin: const EdgeInsets.only(top: 6.0, bottom: 8.0),
-              width: 30.0,
+              width: 40.0,
               height: 6.5,
               decoration: BoxDecoration(
                 color: gray,
